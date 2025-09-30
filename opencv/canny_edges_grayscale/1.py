f8024578d5d0885c3a8a43d24d08d22c849c9570
anurag2506 <anuraldo@gmail.com>
2025-09-30 10:46:00 +0530

    numpy slicing

--- opencv/canny_edges_grayscale/1.py
+++ opencv/canny_edges_grayscale/1.py
@@ -16,27 +16,40 @@ Saves the output as edges.png—without Googling?
 
 import cv2 as cv
 import os
+import numpy as np
+
+# def save_canny(img_path):
+#     file = img_path.split(".")
+#     DIR = os.path.join("dir" + file[0])
+#     os.makedirs(DIR, exist_ok=True)
+#     img = cv.imread(img_path)
+
+#     if img is not None:
+#         cv.imwrite(f"saved_{img_path}", img)
+
+#     try:
+#         gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+#         gray_p = os.path.join(DIR, "gray.png")
+#         cv.imwrite(gray_p, gray)
+#         canny = cv.Canny(gray, 120, 240)
+#         canny_ = os.path.join(DIR, "edges.png")
+#         cv.imwrite(canny_, canny)
+
+#     except Exception as e:
+#         print(f"Error in generating canny edges for {img_path}: {e}")
 
 
-def save_canny(img_path):
-    file = img_path.split(".")
-    DIR = os.path.join("dir" + file[0])
-    os.makedirs(DIR, exist_ok=True)
-    img = cv.imread(img_path)
+# save_canny("./bron.png")
 
+def draw_canny(img_path):
+    img = np.zeros(shape=(100,100,3))
+    img[:5,:] = [0,0,255]
+    img[:,:5] = [0,0,255]
+    img[-5:,:] = [0,0,255]
+    img[:,-5:] = [0,0,255]
+    
     if img is not None:
-        cv.imwrite(f"saved_{img_path}", img)
-
-    try:
-        gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-        gray_p = os.path.join(DIR, "gray.png")
-        cv.imwrite(gray_p, gray)
-        canny = cv.Canny(gray, 120, 240)
-        canny_ = os.path.join(DIR, "edges.png")
-        cv.imwrite(canny_, canny)
-
-    except Exception as e:
-        print(f"Error in generating canny edges for {img_path}: {e}")
-
-
-save_canny("./bron.png")
+        cv.imwrite("red_border.png",img)
+    print(f"Imag saved")
+    
+draw_canny('./bron.png')
